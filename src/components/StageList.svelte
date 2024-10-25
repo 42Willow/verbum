89dd2f0258be8bbbbd7a6be9a24144d974cee04e
willow <42willow@pm.me>
2024-10-25 18:59:11 +1100

    feat: scuffed stage list

--- src/components/StageList.svelte
+++ src/components/StageList.svelte
@@ -1,49 +1,76 @@
+<script>
+  import { onMount } from 'svelte';
+
+  let stagesSelected = "0".repeat(41);
+  let canSelectStages = true;
+  let hStageButtons = [];
+
+ // @ts-ignore
+   $: displayStageColours(); // will be called whenever stagesSelected changes
+
+  function displayStageColours() {
+    for (let i = 0; i < 41; i++) {
+      if (hStageButtons[i]) {
+        hStageButtons[i].style.backgroundColor = stagesSelected[i] === "0" ? "#313244" : "#11111b";
+      }
+    }
+  }
+
+  // @ts-ignore
+  function handleStageClick(event, index) {
+    console.log("handleStageClick", event, index);
+    if (!canSelectStages) return;
+
+    if (event.shiftKey) {
+      if (stagesSelected[index] === "0") {
+        stagesSelected = "1".repeat(index + 1) + stagesSelected.substring(index + 1);
+      } else {
+        stagesSelected = "0".repeat(index + 1) + stagesSelected.substring(index + 1);
+      }
+    } else {
+      if (stagesSelected[index] === "0") {
+        stagesSelected = stagesSelected.substring(0, index) + "1" + stagesSelected.substring(index + 1);
+      } else {
+        stagesSelected = stagesSelected.substring(0, index) + "0" + stagesSelected.substring(index + 1);
+      }
+    }
+    displayStageColours();
+    // TODO: reinitialize the test
+  }
+
+  onMount(() => {
+    displayStageColours();
+    // @ts-ignore
+    hStageButtons = document.querySelectorAll(".stage-button");
+  });
+</script>
+
 <div class="stage-lists">
   <!-- 1-20 -->
   <div class="stage-list">
-    <div class="stage-button">1</div>
-    <div class="stage-button">2</div>
-    <div class="stage-button">3</div>
-    <div class="stage-button">4</div>
-    <div class="stage-button">5</div>
-    <div class="stage-button">6</div>
-    <div class="stage-button">7</div>
-    <div class="stage-button">8</div>
-    <div class="stage-button">9</div>
-    <div class="stage-button">10</div>
-    <div class="stage-button">11</div>
-    <div class="stage-button">12</div>
-    <div class="stage-button">13</div>
-    <div class="stage-button">14</div>
-    <div class="stage-button">15</div>
-    <div class="stage-button">16</div>
-    <div class="stage-button">17</div>
-    <div class="stage-button">18</div>
-    <div class="stage-button">19</div>
-    <div class="stage-button">20</div>
+    {#each Array.from({ length: 20 }, (_, i) => i + 1) as stage}
+      <!-- svelte-ignore a11y_click_events_have_key_events -->
+      <!-- svelte-ignore a11y_no_static_element_interactions -->
+      <div 
+        class="stage-button"
+        on:click={(event) => handleStageClick(event, stage-1)}
+      >
+      {stage}
+      </div>
+    {/each}
   </div>
   <!-- 21-40 -->
   <div class="stage-list">
-    <div class="stage-button">21</div>
-    <div class="stage-button">22</div>
-    <div class="stage-button">23</div>
-    <div class="stage-button">24</div>
-    <div class="stage-button">25</div>
-    <div class="stage-button">26</div>
-    <div class="stage-button">27</div>
-    <div class="stage-button">28</div>
-    <div class="stage-button">29</div>
-    <div class="stage-button">30</div>
-    <div class="stage-button">31</div>
-    <div class="stage-button">32</div>
-    <div class="stage-button">33</div>
-    <div class="stage-button">34</div>
-    <div class="stage-button">35</div>
-    <div class="stage-button">36</div>
-    <div class="stage-button">37</div>
-    <div class="stage-button">38</div>
-    <div class="stage-button">39</div>
-    <div class="stage-button">40</div>
+    {#each Array.from({ length: 20 }, (_, i) => i + 21) as stage}
+      <!-- svelte-ignore a11y_click_events_have_key_events -->
+      <!-- svelte-ignore a11y_no_static_element_interactions -->
+      <div 
+        class="stage-button"
+        on:click={(event) => handleStageClick(event, stage-1)}
+      >
+      {stage}
+      </div>
+    {/each}
   </div>
   <div class="stage-button stage-41-button">extra list</div>
 </div>
